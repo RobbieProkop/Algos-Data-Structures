2eea843ac39350f06743516ee4daa62ca3346b09
Robbie Prokop <robbieprokop@gmail.com>
2023-09-08 14:45:42 -0600

    add another solution for 07

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,13 @@
-function reverseString() {}
+function reverseString(str) {
+  // let reversed = [];
+
+  // for (let i = str.length; i >= 0; i--) {
+  //   reversed.push(str[i]);
+  // }
+
+  // return reversed.join("");
+
+  return str.split("").reverse().join("");
+}
 
 module.exports = reverseString;
