f08ca80a22499f000d5fd5863174860b2d22fec0
Robbie Prokop <robbieprokop@gmail.com>
2023-09-08 14:28:13 -0600

    new solution for 04

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,6 +1,31 @@
 function calculator(a, b, c) {
+  if (c !== "+" && c !== "-" && c !== "*" && c !== "/")
+    throw new Error("Invalid Operator");
   return eval(a + c + b);
 }
+
+// function calculator(num1, num2, operator) {
+//   let result;
+
+//   switch (operator) {
+//     case "+":
+//       result = num1 + num2;
+//       break;
+//     case "-":
+//       result = num1 - num2;
+//       break;
+//     case "*":
+//       result = num1 * num2;
+//       break;
+//     case "/":
+//       result = num1 / num2;
+//       break;
+//     default:
+//       throw new Error("invalid operator");
+//   }
+
+//   return result;
+// }
 
 console.log("calculate", calculator(2, 2, "+"));
 
--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,13 +1,19 @@
+//with for loop
+
+// function countOccurrences(word, letter) {
+//   let count = 0;
+//   for (let i = 0; i <= word.length; i++) {
+//     if (word[i] === letter) {
+//       count += 1;
+//     }
+//   }
+
+//   return count;
+// }
+
+// with split.length
 function countOccurrences(word, letter) {
-  let count = 0;
-  for (let i = 0; i <= word.length; i++) {
-    if (word[i] === letter) {
-      count += 1;
-    }
-  }
-
-  return count;
+  let newWord = word.split(letter).length - 1;
+  return newWord;
 }
-
-console.log("total", countOccurrences("hello", "l"));
 module.exports = countOccurrences;
