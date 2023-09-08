2b4987ae04b84d01604524e4c25a5f6504e11e89
Robbie Prokop <robbieprokop@gmail.com>
2023-09-08 13:46:23 -0600

    solve 04

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,13 @@
-function countOccurrences() {}
+function countOccurrences(word, letter) {
+  let count = 0;
+  for (let i = 0; i <= word.length; i++) {
+    if (word[i] === letter) {
+      count += 1;
+    }
+  }
 
+  return count;
+}
+
+console.log("total", countOccurrences("hello", "l"));
 module.exports = countOccurrences;
