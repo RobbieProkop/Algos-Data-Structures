8b0f2ac2b6dcbbda7280e52db42afe9de41c10d4
Robbie Prokop <robbieprokop@gmail.com>
2023-09-08 14:37:47 -0600

    solve 06

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,22 +1,10 @@
 function titleCase(string) {
-  let newString = string.toLowerCase();
-
-  newString = newString.split(" ");
-  for (let i = 0; i < newString.length; i++) {
-    // console.log(newString[i][0].toUpperCase());
-
-    for (let j = 0; j < newString[i].length; j++) {
-      if (j === 0) {
-        newString[i] = newString[i][j].toUpperCase();
-      }
-      newString[i] += newString[i][j];
-    }
-
-    console.log(newString[i]);
+  let wordArr = string.toLowerCase().split(" ");
+  for (let i = 0; i < wordArr.length; i++) {
+    wordArr[i] = wordArr[i][0].toUpperCase() + wordArr[i].slice(1);
   }
 
-  return newString;
+  return wordArr.join(" ");
 }
 
-console.log("title", titleCase("oH mY gOsH"));
 module.exports = titleCase;
