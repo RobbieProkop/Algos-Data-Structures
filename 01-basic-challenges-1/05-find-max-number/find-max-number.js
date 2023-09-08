2d65a5ec82d53625ccb662db394cb53596272905
Robbie Prokop <robbieprokop@gmail.com>
2023-09-08 13:49:21 -0600

    solve 05

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(numArray) {
+  let biggest = 0;
 
+  for (let i = 0; i <= numArray.length; i++) {
+    if (numArray[i] > biggest) biggest = numArray[i];
+  }
+  return biggest;
+}
+
+console.log("biggest", findMaxNumber([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]));
 module.exports = findMaxNumber;
