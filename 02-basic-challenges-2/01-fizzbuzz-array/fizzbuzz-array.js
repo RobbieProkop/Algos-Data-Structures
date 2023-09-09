222c8ba227772aa28e6d4acb27fc4654f85a0223
Robbie Prokop <robbieprokop@gmail.com>
2023-09-09 15:29:34 -0600

    solvew fizzbuzz with switch and ifelse

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,35 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  let arr = [];
+
+  for (let i = 1; i <= num; i++) {
+    const eval3 = i % 3 === 0;
+    const eval5 = i % 5 === 0;
+    switch (true) {
+      case eval3 && eval5:
+        arr.push("FizzBuzz");
+        break;
+      case eval3:
+        arr.push("Fizz");
+        break;
+      case eval5:
+        arr.push("Buzz");
+        break;
+      default:
+        arr.push(i);
+        break;
+    }
+    // if (i % 3 === 0 && i % 5 === 0) {
+    //   arr.push("FizzBuzz");
+    // } else if (i % 3 === 0) {
+    //   arr.push("Fizz");
+    // } else if (i % 5 === 0) {
+    //   arr.push("Buzz");
+    // } else {
+    //   arr.push(i);
+    // }
+  }
+
+  return arr;
+}
 
 module.exports = fizzBuzzArray;
