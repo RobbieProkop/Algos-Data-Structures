1e77b6a392afab0f3d8c099bcf423676769e8a40
Robbie Prokop <robbieprokop@gmail.com>
2023-09-08 19:14:51 -0600

    solve 08

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,6 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  const noSpaceNoComma = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  return noSpaceNoComma.split("").reverse().join("") === noSpaceNoComma;
+}
 
 module.exports = isPalindrome;
