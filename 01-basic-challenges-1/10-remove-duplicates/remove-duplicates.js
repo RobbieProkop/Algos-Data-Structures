07b46416c0ea911c247aa6a7422486a705a00d51
Robbie Prokop <robbieprokop@gmail.com>
2023-09-08 19:52:20 -0600

    add Set solution for 10

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,16 +1,20 @@
+// function removeDuplicates(arr) {
+//   let newArr = [];
+//   //option 1
+//   // for (let i = 0; i < arr.length; i++) {
+//   //   if (!newArr.includes(arr[i])) {
+//   //     newArr.push(arr[i]);
+//   //   }
+//   // }
+//   arr.forEach((item) => {
+//     if (!newArr.includes(item)) {
+//       newArr.push(item);
+//     }
+//   });
+//   return newArr;
+// }
 function removeDuplicates(arr) {
-  let newArr = [];
-  // for (let i = 0; i < arr.length; i++) {
-  //   if (!newArr.includes(arr[i])) {
-  //     newArr.push(arr[i]);
-  //   }
-  // }
-  arr.forEach((item) => {
-    if (!newArr.includes(item)) {
-      newArr.push(item);
-    }
-  });
-  return newArr;
+  return Array.from(new Set(arr));
 }
 
 module.exports = removeDuplicates;
