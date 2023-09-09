13b9c67f16096507708bc62cc885d79fec64e04e
Robbie Prokop <robbieprokop@gmail.com>
2023-09-09 16:06:31 -0600

    create switch solution

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,23 +1,33 @@
 function displayLikes(arr) {
-  let likesStr = "";
-  if (arr.length === 0) {
-    likesStr = "no one likes this";
-  }
+  const arrLength = arr.length;
+  // if (arrLength === 0) {
+  //   return "no one likes this";
+  // }
+  // if (arrLength === 1) {
+  //   return `${arr[0]} likes this`;
+  // }
+  // if (arrLength === 2) {
+  //   return `${arr[0]} and ${arr[1]} like this`;
+  // }
+  // if (arrLength === 3) {
+  //   return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+  // }
+  // if (arrLength > 3) {
+  //   return `${arr[0]}, ${arr[1]} and ${arrLength - 2} others like this`;
+  // }
 
-  if (arr.length === 1) {
-    likesStr = `${arr[0]} likes this`;
+  switch (arrLength) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${arr[0]} likes this`;
+    case 2:
+      return `${arr[0]} and ${arr[1]} like this`;
+    case 3:
+      return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+    default:
+      return `${arr[0]}, ${arr[1]} and ${arrLength - 2} others like this`;
   }
-  if (arr.length === 2) {
-    likesStr = `${arr[0]} and ${arr[1]} like this`;
-  }
-  if (arr.length === 3) {
-    likesStr = `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
-  }
-  if (arr.length > 3) {
-    likesStr = `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
-  }
-
-  return likesStr;
 }
 
 module.exports = displayLikes;
