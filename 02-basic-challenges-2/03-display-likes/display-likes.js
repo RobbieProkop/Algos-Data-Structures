6ab478eefd4cb656ff0ea07f777260924a123f9c
Robbie Prokop <robbieprokop@gmail.com>
2023-09-09 15:58:02 -0600

    solve 02-03 with ifs

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,23 @@
-function displayLikes() {}
+function displayLikes(arr) {
+  let likesStr = "";
+  if (arr.length === 0) {
+    likesStr = "no one likes this";
+  }
+
+  if (arr.length === 1) {
+    likesStr = `${arr[0]} likes this`;
+  }
+  if (arr.length === 2) {
+    likesStr = `${arr[0]} and ${arr[1]} like this`;
+  }
+  if (arr.length === 3) {
+    likesStr = `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+  }
+  if (arr.length > 3) {
+    likesStr = `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
+  }
+
+  return likesStr;
+}
 
 module.exports = displayLikes;
