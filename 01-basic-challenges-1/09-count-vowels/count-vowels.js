9fada863031ce667c675c5b4dd387f58541aba89
Robbie Prokop <robbieprokop@gmail.com>
2023-09-08 19:48:35 -0600

    solve 8-10

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,21 @@
-function countVowels() {}
+function countVowels(str) {
+  let vowelCount = 0;
+  const lower = str.toLowerCase();
+
+  for (let i = 0; i < lower.length; i++) {
+    if (
+      lower[i] === "a" ||
+      lower[i] === "e" ||
+      lower[i] === "i" ||
+      lower[i] === "o" ||
+      lower[i] === "u" ||
+      lower[i] === "y"
+    ) {
+      vowelCount++;
+    }
+  }
+
+  return vowelCount;
+}
 
 module.exports = countVowels;
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,16 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  let newArr = [];
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (!newArr.includes(arr[i])) {
+  //     newArr.push(arr[i]);
+  //   }
+  // }
+  arr.forEach((item) => {
+    if (!newArr.includes(item)) {
+      newArr.push(item);
+    }
+  });
+  return newArr;
+}
 
 module.exports = removeDuplicates;
