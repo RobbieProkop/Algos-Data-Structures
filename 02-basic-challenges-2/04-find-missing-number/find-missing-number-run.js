1bc37f48ed007ede7d54ca16c38f1849d4b808b2
Robbie Prokop <robbieprokop@gmail.com>
2023-09-09 16:36:12 -0600

    solve 02 - 04

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number-run.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number-run.js
@@ -1,5 +1,6 @@
-const findMissingNumber = require('./find-missing-number');
+const findMissingNumber = require("./find-missing-number");
 
-const result = findMissingNumber([10, 8, 6, 7, 5, 4, 2, 3, 1]);
+// const result = findMissingNumber([10, 8, 6, 7, 5, 4, 2, 3, 1]);
+const result = findMissingNumber([1, 3, 4, 5, 6]);
 
 console.log(result);
--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,16 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  let missing;
+  let current = 1;
+  const sorted = arr.sort((a, b) => a - b);
+
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] !== current) {
+      missing = i + 1;
+      return missing;
+    }
+    current += 1;
+  }
+  return missing;
+}
 
 module.exports = findMissingNumber;
