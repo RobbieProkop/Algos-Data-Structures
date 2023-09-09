328f583e3140bbb09971438a3a1d83267e349b65
Robbie Prokop <robbieprokop@gmail.com>
2023-09-09 15:41:43 -0600

    solve 02-02

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,15 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const resultArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        resultArr.push(arr1[i]);
+      }
+    }
+  }
+
+  return resultArr;
+}
 
 module.exports = arrayIntersection;
