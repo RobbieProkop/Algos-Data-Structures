e79cbe258db293245acf70747807272da3be0710
Robbie Prokop <robbieprokop@gmail.com>
2023-09-09 16:48:56 -0600

    add different solution

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number-run.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number-run.js
@@ -1,6 +1,6 @@
 const findMissingNumber = require("./find-missing-number");
 
 // const result = findMissingNumber([10, 8, 6, 7, 5, 4, 2, 3, 1]);
-const result = findMissingNumber([1, 3, 4, 5, 6]);
+const result = findMissingNumber([1, 2, 3, 5]);
 
 console.log(result);
--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,15 +1,22 @@
 function findMissingNumber(arr) {
   let missing;
-  let current = 1;
-  const sorted = arr.sort((a, b) => a - b);
+  // let current = 1;
+  // const sorted = arr.sort((a, b) => a - b);
 
-  for (let i = 0; i < sorted.length; i++) {
-    if (sorted[i] !== current) {
-      missing = i + 1;
-      return missing;
-    }
-    current += 1;
-  }
+  // for (let i = 0; i < sorted.length; i++) {
+  //   if (sorted[i] !== current) {
+  //     missing = i + 1;
+  //     return missing;
+  //   }
+  //   current += 1;
+  // }
+  let n = arr.length + 1;
+  let expected = (n * (n + 1)) / 2;
+  let actual = 0;
+  arr.forEach((num) => {
+    actual += num;
+  });
+  missing = expected - actual;
   return missing;
 }
 
