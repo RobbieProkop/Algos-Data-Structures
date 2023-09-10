3ab518a0dd286f649f5656fe437e400d08a64a83
Robbie Prokop <robbieprokop@gmail.com>
2023-09-09 20:53:09 -0600

    use for of loops for 02-07

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,4 +1,13 @@
 function findFirstNonRepeatingCharacter(str) {
+  // const charCount = {};
+  // for (const char of str) {
+  //   charCount[char] = (charCount[char] || 0) + 1;
+  // }
+  // for (const char of str) {
+  //   if (charCount[char] === 1) return char;
+  // }
+  // return null;
+
   for (let i = 0; i < str.length; i++) {
     if (str[i] !== str[i + 1] && str[i] !== str[i - 1]) return str[i];
   }
