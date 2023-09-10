20395716729d913a2ec1158751901da609f579ad
Robbie Prokop <robbieprokop@gmail.com>
2023-09-09 19:55:57 -0600

    add 3rd solution

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -15,13 +15,25 @@ function findMissingLetter(arr) {
   // });
   // return missing[0];
 
-  const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const startIndex = letters.indexOf(arr[0]);
+  //solution 2
+  // const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // const startIndex = letters.indexOf(arr[0]);
 
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr[i] !== letters[startIndex + i]) {
+  //     return letters[startIndex + i];
+  //   }
+  // }
+  // return "";
+
+  //solution 3 (better version of #1)
+  let start = arr[0].charCodeAt(0);
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== letters[startIndex + i]) {
-      return letters[startIndex + i];
+    const current = arr[i].charCodeAt(0);
+    if (current - start > 1) {
+      return String.fromCharCode(start + 1);
     }
+    start = current;
   }
   return "";
 }
