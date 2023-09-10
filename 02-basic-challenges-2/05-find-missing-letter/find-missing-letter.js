48c385d1f1a19d87832056934df8d777783df57c
Robbie Prokop <robbieprokop@gmail.com>
2023-09-09 19:51:16 -0600

    solution using indeOf

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,19 +1,29 @@
 function findMissingLetter(arr) {
-  if (arr.length === 0) return [];
+  // if (arr.length === 0) return [];
 
-  let newArr = [];
-  let missing = [""];
-  arr.forEach((letter) => {
-    const code = letter.charCodeAt(0);
-    newArr.push(code);
-  });
-  let currentNum = newArr[0];
-  newArr.forEach((num) => {
-    if (num !== currentNum && num !== currentNum - 1)
-      return (missing = [String.fromCharCode(currentNum)]);
-    currentNum++;
-  });
-  return missing[0];
+  // let newArr = [];
+  // let missing = [""];
+  // arr.forEach((letter) => {
+  //   const code = letter.charCodeAt(0);
+  //   newArr.push(code);
+  // });
+  // let currentNum = newArr[0];
+  // newArr.forEach((num) => {
+  //   if (num !== currentNum && num !== currentNum - 1)
+  //     return (missing = [String.fromCharCode(currentNum)]);
+  //   currentNum++;
+  // });
+  // return missing[0];
+
+  const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const startIndex = letters.indexOf(arr[0]);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== letters[startIndex + i]) {
+      return letters[startIndex + i];
+    }
+  }
+  return "";
 }
 
 module.exports = findMissingLetter;
