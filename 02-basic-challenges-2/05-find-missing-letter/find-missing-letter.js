0ef97aa6b1ead070d3962c1bea8ed2ead1811428
Robbie Prokop <robbieprokop@gmail.com>
2023-09-09 18:23:13 -0600

    find missing letter solution

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,19 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  if (arr.length === 0) return [];
+
+  let newArr = [];
+  let missing = [];
+  arr.forEach((letter) => {
+    const code = letter.charCodeAt(0);
+    newArr.push(code);
+  });
+  let currentNum = newArr[0];
+  newArr.forEach((num) => {
+    if (num !== currentNum && num !== currentNum - 1)
+      return missing.push(String.fromCharCode(currentNum));
+    currentNum++;
+  });
+  return missing[0];
+}
 
 module.exports = findMissingLetter;
