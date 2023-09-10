070b52c7f4105ce812a9908fab78bd5b22f26830
Robbie Prokop <robbieprokop@gmail.com>
2023-09-10 13:22:12 -0600

    solve 02-10

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,13 @@
-function validateEmail() {}
+function validateEmail(email) {
+  if (email.length < 6) return false;
+
+  if (!email.includes("@") || !email.includes(".")) return false;
+  const first = email.split("@");
+  const middle = first[1].split(".")[0];
+  const end = email.split(".")[1];
+  if (first[0].length < 1 || middle.length < 1 || end.length < 2) return false;
+
+  return true;
+}
 
 module.exports = validateEmail;
