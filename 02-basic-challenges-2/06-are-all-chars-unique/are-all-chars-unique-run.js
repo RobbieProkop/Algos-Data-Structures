7862c7d78f61271356293259e81970fd4fa00406
Robbie Prokop <robbieprokop@gmail.com>
2023-09-09 20:22:26 -0600

    solve o2-06 with 3 solutions

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique-run.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique-run.js
@@ -1,9 +1,11 @@
-const areAllCharactersUnique = require('./are-all-chars-unique');
+const areAllCharactersUnique = require("./are-all-chars-unique");
 
-const result1 = areAllCharactersUnique('abcdefg');
-const result2 = areAllCharactersUnique('abcdefgA');
-const result3 = areAllCharactersUnique('programming');
+const result1 = areAllCharactersUnique("abcdefg");
+const result2 = areAllCharactersUnique("abcdefgA");
+const result3 = areAllCharactersUnique("programming");
+const result4 = areAllCharactersUnique("");
 
 console.log(result1);
 console.log(result2);
 console.log(result3);
+console.log(result4);
--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,24 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  // let arr = [];
+  // for (let i = 0; i < str.length; i++) {
+  //   arr.push(str[i]);
+  // }
+  // let newArr = Array.from(new Set(arr));
+  // return arr.length === newArr.length;
+
+  // const charCount = {};
+  // for (let i = 0; i < str.length; i++) {
+  //   if (charCount[str[i]]) return false;
+  //   charCount[str[i]] = true;
+  // }
+  // return true;
+
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (arr.includes(str[i])) return false;
+    arr.push(str[i]);
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
