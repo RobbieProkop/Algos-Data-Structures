d8adfb0048fa9e7ce0e9f43280dca3570424a38d
Robbie Prokop <robbieprokop@gmail.com>
2023-09-10 14:10:14 -0600

    solve 03-02

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares-run.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares-run.js
@@ -1,5 +1,9 @@
-const sumOfEvenSquares = require('./sum-of-even-squares');
+const sumOfEvenSquares = require("./sum-of-even-squares");
 
 const result = sumOfEvenSquares([1, 2, 3, 4, 5, 6]);
+const result2 = sumOfEvenSquares([]);
+const result3 = sumOfEvenSquares([-11, 2]);
 
 console.log(result);
+console.log(result2);
+console.log(result3);
--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,8 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(nums) {
+  const even = nums.filter((num) => num % 2 === 0);
+  const squared = even.map((num) => num * num);
+  const total = squared.reduce((prev, cur) => prev + cur, 0);
+  return total;
+}
 
 module.exports = sumOfEvenSquares;
