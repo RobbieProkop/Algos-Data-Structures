4031c06f5a8aa4c570c85582ed23ddd0442cb983
Robbie Prokop <robbieprokop@gmail.com>
2023-09-09 20:39:09 -0600

    solve 02-07

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,8 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i + 1] && str[i] !== str[i - 1]) return str[i];
+  }
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
