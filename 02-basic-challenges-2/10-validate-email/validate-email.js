e80f3b4fa96e8acbdfe448ce13b92eabc3e7f552
Robbie Prokop <robbieprokop@gmail.com>
2023-09-10 13:24:20 -0600

    add regex solution for email

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,12 +1,18 @@
 function validateEmail(email) {
-  if (email.length < 6) return false;
+  // if (email.length < 6) return false;
 
-  if (!email.includes("@") || !email.includes(".")) return false;
-  const first = email.split("@");
-  const middle = first[1].split(".")[0];
-  const end = email.split(".")[1];
-  if (first[0].length < 1 || middle.length < 1 || end.length < 2) return false;
+  // if (!email.includes("@") || !email.includes(".")) return false;
+  // const first = email.split("@");
+  // const middle = first[1].split(".")[0];
+  // const end = email.split(".")[1];
+  // if (first[0].length < 1 || middle.length < 1 || end.length < 2) return false;
 
+  // return true;
+
+  //solution 2
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(email)) return false;
   return true;
 }
 
