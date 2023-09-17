c05328387ed13e6dd7ad3957fbe7653157ca6f12
Robbie Prokop <robbieprokop@gmail.com>
2023-09-16 18:29:19 -0600

    not solved yet

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,24 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+  let words = str.split(" ");
+  let highestScore = 0;
+  let values = [
+    'a', 'b', 'c', 'd', 'e', 
+    'f', 'g', 'h', 'i', 'j', 
+    'k', 'l', 'm', 'n', 'o', 
+    'p', 'q', 'r', 's', 't', 
+    'u', 'v', 'w', 'x', 'y', 
+    'z'
+  ];
+  
+
+  words.forEach((word, index) => {
+    let letters = word.split("");
+    let total = 0;
+    letters.forEach(letter => {
+      if (letters[] === values[index])
+    })
+ 
+  });
+}
 
 module.exports = highestScoringWord;
