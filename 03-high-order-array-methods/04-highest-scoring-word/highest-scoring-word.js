7d8d5839b698deb0f0c65e5d824c5b7b7e1da156
Robbie Prokop <robbieprokop@gmail.com>
2023-11-26 21:48:36 -0700

    3rd solujtion for highest score

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -17,27 +17,42 @@
 //   return highestWord;
 // }
 
+// function highestScoringWord(str) {
+//   let words = str.split(" ");
+
+//   const scores = words.map((word) => {
+//     let score = 0;
+//     for (const letter of word) {
+//       score += letter.charCodeAt(0) - 96;
+//     }
+//     return score;
+//   });
+//   let highestScore = scores[0];
+//   let highestWord = words[0];
+
+//   scores.forEach((score, index) => {
+//     if (score > highestScore) {
+//       highestScore = scores[index];
+//       highestWord = words[index];
+//     }
+//   });
+
+//   return highestWord;
+// }
+
 function highestScoringWord(str) {
   let words = str.split(" ");
 
   const scores = words.map((word) => {
-    let score = 0;
-    for (const letter of word) {
-      score += letter.charCodeAt(0) - 96;
-    }
-    return score;
+    return Array.from(word).reduce(
+      (score, letter) => score + letter.charCodeAt(0) - 96,
+      0
+    );
   });
-  let highestScore = scores[0];
-  let highestWord = words[0];
+  let highestScore = Math.max(...scores);
+  let highestWord = scores.indexOf(highestScore);
 
-  scores.forEach((score, index) => {
-    if (score > highestScore) {
-      highestScore = scores[index];
-      highestWord = words[index];
-    }
-  });
-
-  console.log(highestWord);
+  return words[highestWord];
 }
 
 module.exports = highestScoringWord;
