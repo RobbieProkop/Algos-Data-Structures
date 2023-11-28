2e0f0a242c69c0ed71a0e992884cee9221948265
Robbie Prokop <robbieprokop@gmail.com>
2023-11-28 11:50:08 -0700

    use regex to validate password

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,7 @@
-function validatePassword() {}
+function validatePassword(str) {
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/;
+  if (str.length < 8 || !regex.test(str)) return false;
+  return true;
+}
 
 module.exports = validatePassword;
