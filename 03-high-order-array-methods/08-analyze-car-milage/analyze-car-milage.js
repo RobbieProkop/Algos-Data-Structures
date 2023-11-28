15ee8c16efc0c2a5cfbb7be2abb96b62af5fe2b6
Robbie Prokop <robbieprokop@gmail.com>
2023-11-28 11:32:29 -0700

    create 2nd solution for analyze car

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,27 +1,50 @@
+// function analyzeCarMileage(cars) {
+//   let highestMileageCar = cars[0];
+//   let lowestMileageCar = cars[0];
+
+//   let totalMileage = 0;
+
+//   cars.forEach((car) => {
+//     if (car.mileage > highestMileageCar.mileage) {
+//       highestMileageCar = car;
+//     }
+//     if (car.mileage < lowestMileageCar.mileage) {
+//       lowestMileageCar = car;
+//     }
+//     totalMileage += car.mileage;
+//   });
+
+//   let averageMileage = Number((totalMileage / cars.length).toFixed(2));
+
+//   return {
+//     averageMileage,
+//     highestMileageCar,
+//     lowestMileageCar,
+//     totalMileage,
+//   };
+// }
+
 function analyzeCarMileage(cars) {
-  let highestMileageCar = cars[0];
-  let lowestMileageCar = cars[0];
+  const result = cars.reduce(
+    (res, car) => {
+      if (car.mileage > res.highestMileageCar.mileage)
+        res.highestMileageCar = car;
 
-  let totalMileage = 0;
+      if (car.mileage < res.lowestMileageCar.mileage)
+        res.lowestMileageCar = car;
 
-  cars.forEach((car) => {
-    if (car.mileage > highestMileageCar.mileage) {
-      highestMileageCar = car;
+      res.totalMileage += car.mileage;
+      res.averageMileage = Number((res.totalMileage / cars.length).toFixed(2));
+      return res;
+    },
+    {
+      highestMileageCar: cars[0],
+      lowestMileageCar: cars[0],
+      totalMileage: 0,
+      averageMileage: 0,
     }
-    if (car.mileage < lowestMileageCar.mileage) {
-      lowestMileageCar = car;
-    }
-    totalMileage += car.mileage;
-  });
-
-  let averageMileage = Number((totalMileage / cars.length).toFixed(2));
-
-  return {
-    averageMileage,
-    highestMileageCar,
-    lowestMileageCar,
-    totalMileage,
-  };
+  );
+  return result;
 }
 
 module.exports = analyzeCarMileage;
