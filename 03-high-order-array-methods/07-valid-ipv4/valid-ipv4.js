40c45f5e8e94dc4381759a06aba18ce0edc59f00
Robbie Prokop <robbieprokop@gmail.com>
2023-11-28 10:57:11 -0700

    found first solution for analyze car

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,12 +1,10 @@
 const isValidIPv4 = (str) => {
   const split = str.split(".");
-
   const isValid =
     split.length === 4 &&
     split.every((octet) =>
       octet < 1 || octet > 255 || octet.split("")[0] === "0" ? false : true
     );
-
   return isValid;
 };
 
--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,27 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  let highestMileageCar = cars[0];
+  let lowestMileageCar = cars[0];
+
+  let totalMileage = 0;
+
+  cars.forEach((car) => {
+    if (car.mileage > highestMileageCar.mileage) {
+      highestMileageCar = car;
+    }
+    if (car.mileage < lowestMileageCar.mileage) {
+      lowestMileageCar = car;
+    }
+    totalMileage += car.mileage;
+  });
+
+  let averageMileage = Number((totalMileage / cars.length).toFixed(2));
+
+  return {
+    averageMileage,
+    highestMileageCar,
+    lowestMileageCar,
+    totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
