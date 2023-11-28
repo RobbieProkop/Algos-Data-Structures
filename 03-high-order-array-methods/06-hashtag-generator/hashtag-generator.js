9c6fc40b41c874de64f4200593877fee9c7974d9
Robbie Prokop <robbieprokop@gmail.com>
2023-11-27 18:29:05 -0700

    first solution for hashtag generator

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,22 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (str.length > 140) return false;
+  if (str === "") return false;
+
+  const regex = /\s+/g;
+
+  let hashed = str.replace(regex, " ").trim().split(" ");
+
+  const upper = hashed.map((word) => {
+    let letters = word.split("");
+    letters[0] = letters[0].toUpperCase();
+    letters = letters.join("");
+    return letters;
+  });
+
+  upper.splice(0, 0, "#");
+  hashed = upper.join("");
+
+  return hashed;
+}
 
 module.exports = generateHashtag;
