90c2da3e3b09069eabb9078808dff94c08d2c92a
Robbie Prokop <robbieprokop@gmail.com>
2023-11-28 12:24:36 -0700

    find solution for missing letter

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,11 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  let missing;
+  const unicodes = arr.map((letter) => letter.charCodeAt());
+  unicodes.filter((num, index) => {
+    if (num !== unicodes[0] && num !== unicodes[index - 1] + 1)
+      missing = String.fromCharCode(num - 1);
+  });
+  return missing;
+}
 
 module.exports = findMissingLetter;
