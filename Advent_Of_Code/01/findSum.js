6e0f3cac1587699369a29d44c40520b473af5e40
Robbie Prokop <robbieprokop@gmail.com>
2023-12-03 14:45:34 -0700

    refactored findSUm advent

--- Advent_Of_Code/01/findSum.js
+++ Advent_Of_Code/01/findSum.js
@@ -1,16 +1,27 @@
-const findSum = (str) => {
-  const arr = str.split("\n");
-  let numArr = [];
-  arr.forEach((newStr) => {
-    const first = newStr.split("").find((char) => !isNaN(char));
-    const last = newStr
-      .split("")
-      .reverse()
-      .find((char) => !isNaN(char));
+// const findSum = (str) => {
+//   const arr = str.split("\n");
+//   let numArr = [];
+//   arr.forEach((newStr) => {
+//     const first = newStr.split("").find((char) => !isNaN(char));
+//     const last = newStr
+//       .split("")
+//       .reverse()
+//       .find((char) => !isNaN(char));
 
-    numArr.push(`${first}${last}`);
+//     numArr.push(`${first}${last}`);
+//   });
+//   console.log("numArr :>> ", numArr);
+//   const sum = numArr.reduce((prev, cur) => Number(prev) + Number(cur), 0);
+//   console.log("sum :>> ", sum);
+//   return sum;
+// };
+
+const findSum = (str) => {
+  const numArr = str.split("\n").map((subStr) => {
+    const first = [...subStr].find((num) => !isNaN(num));
+    const last = [...subStr].reverse().find((num) => !isNaN(num));
+    return `${first}${last}`;
   });
-  console.log("numArr :>> ", numArr);
   const sum = numArr.reduce((prev, cur) => Number(prev) + Number(cur), 0);
   console.log("sum :>> ", sum);
   return sum;
