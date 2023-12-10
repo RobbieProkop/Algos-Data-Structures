5c80cde66b4338cd26f5c817d0644ce1216a0431
Robbie Prokop <robbieprokop@gmail.com>
2023-12-09 21:22:00 -0700

    recursion 1

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,8 +1,19 @@
 function countDown(num) {
-  while (num > 0) {
-    console.log("num :>> ", num--);
-    num--;
+  // while (num > 0) {
+  //   console.log("num :>> ", num--);
+  //   num--;
+  // }
+
+  // base case
+  if (num <= 0) {
+    console.log("num :>> ", num);
+    return;
   }
+
+  //recursion
+  console.log("num :>> ", num);
+  num--;
+  countDown(num);
 }
 
 module.exports = countDown;
