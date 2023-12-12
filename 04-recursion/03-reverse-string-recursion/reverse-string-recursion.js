eb9a954a473db2af6140d2593d01b5e63f868f5e
Robbie Prokop <robbieprokop@gmail.com>
2023-12-12 12:17:13 -0700

    get reversed str

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+function reverseString(str) {
+  //base case
+  if (str === "") return "";
+  let sub = structuredClone(str);
+  let reversed = sub.slice(1);
+  return reverseString(reversed) + sub[0];
+}
 
 module.exports = reverseString;
