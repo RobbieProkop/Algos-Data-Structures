e640e33f6ddcd886b1bb6d38c2959ed7d60f6aa1
Robbie Prokop <robbieprokop@gmail.com>
2023-12-14 21:45:03 -0700

    difficult flattenarray

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,27 @@
-function flattenArray() {}
+// function flattenArray(arr) {
+//   let flattened = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     // if (typeof arr[i] === "object") {
+//     if (Array.isArray(arr[i])) {
+//       flattened = flattened.concat(flattenArray(arr[i]));
+//     } else {
+//       flattened.push(arr[i]);
+//     }
+//   }
+
+//   return flattened;
+// }
+
+const flattenArray = (arr) => {
+  let flattened = [];
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      flattened = flattened.concat(flattenArray(item));
+    } else {
+      flattened.push(item);
+    }
+  }
+  return flattened;
+};
 
 module.exports = flattenArray;
