e8e01e8839d853b2d81ec8fea6dcc31492f905ad
Robbie Prokop <robbieprokop@gmail.com>
2023-12-14 20:48:38 -0700

    create multiple solutions for arraysum

--- 04-recursion/07-array-sum/array-sum.js
+++ 04-recursion/07-array-sum/array-sum.js
@@ -1,3 +1,20 @@
-function arraySum() {}
+// function arraySum(arr) {
+//   if (arr.length === 0) return 0;
+//   if (arr.length === 1) return arr[0];
 
+//   const new0 = arr[0] + arr[1];
+//   const newArr = arr.slice(2);
+//   newArr.unshift(new0);
+
+//   return arraySum(newArr);
+// }
+
+// function arraySum(arr) {
+//   if (arr.length === 0) return 0;
+
+//   return arr[0] + arraySum(arr.slice(1));
+// }
+
+const arraySum = (arr) =>
+  arr.length === 0 ? 0 : arr[0] + arraySum(arr.slice(1));
 module.exports = arraySum;
