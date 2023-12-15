d30301464cfb2dd40877d86ff81bb1b58aef8a94
Robbie Prokop <robbieprokop@gmail.com>
2023-12-14 22:06:21 -0700

    permutations 😢

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,22 @@
-function permutations() {}
+function permutations(str) {
+  let array = [];
+
+  if (str.length === 0) {
+    array.push("");
+    return array;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    const first = str[i];
+    const remaining = str.slice(0, i) + str.slice(i + 1);
+
+    const subPerm = permutations(remaining);
+
+    for (let j = 0; j < subPerm.length; j++) {
+      array.push(first + subPerm[j]);
+    }
+  }
+  return array;
+}
 
 module.exports = permutations;
