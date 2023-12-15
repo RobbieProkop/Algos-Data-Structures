aa0eefd09b3735b8d3976c5a64a20cdaddf0f094
Robbie Prokop <robbieprokop@gmail.com>
2023-12-14 19:50:35 -0700

    recursive power

--- 04-recursion/06-power/power.js
+++ 04-recursion/06-power/power.js
@@ -1,3 +1,7 @@
-function power() {}
+// function power(num, exp) {
+//   if (exp < 2) return num;
+//   return num * power(num, exp - 1);
+// }
+const power = (num, exp) => (exp < 2 ? num : num * power(num, exp - 1));
 
 module.exports = power;
