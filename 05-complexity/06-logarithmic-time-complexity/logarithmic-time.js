c71840e6e59ccbe1b88456ff2eeb5abacff5a511
Robbie Prokop <robbieprokop@gmail.com>
2023-12-15 21:16:45 -0700

    logrithmic time with find power recursive

--- 05-complexity/06-logarithmic-time-complexity/logarithmic-time.js
+++ 05-complexity/06-logarithmic-time-complexity/logarithmic-time.js
@@ -5,3 +5,31 @@ Logarithmic time means that the time required to complete a function is proporti
 
 */
 
+//Recusrion examples - NOT LOG N
+// const findPower = (base, exp) =>
+//   exp === 0 ? 1 : base * findPower(base, exp - 1);
+// const findPower = (base, exp) => {
+//   if (exp === 0) return 1;
+
+//   console.log("exp :>> ", exp);
+//   return base * findPower(base, exp - 1);
+// };
+
+const findPower = (base, exp) => {
+  if (exp === 0) return 1;
+
+  if (exp % 2 === 0) {
+    const halfPower = findPower(base, exp / 2);
+    return halfPower * halfPower;
+  } else {
+    const halfPower = findPower(base, (exp - 1) / 2);
+    return base * halfPower * halfPower;
+  }
+};
+
+console.time("find power 1");
+findPower(2, 100);
+console.timeEnd("find power 1");
+console.time("find power 2");
+findPower(2, 10);
+console.timeEnd("find power 2");
