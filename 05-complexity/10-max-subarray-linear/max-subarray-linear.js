1d0bb8edc5c56aed2057e39d7684351e81dfb964
Robbie Prokop <robbieprokop@gmail.com>
2023-12-15 22:02:20 -0700

    linear O of max sub array sum

--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -1,3 +1,20 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let current = 0;
+  let max = 0;
+
+  for (let i = 0; i < k; i++) {
+    max += arr[i];
+  }
+
+  current = max;
+
+  for (let j = k; j < arr.length; j++) {
+    current = current - arr[j - k] + arr[j];
+    console.log("current :>> ", current);
+    if (current > max) max = current;
+  }
+
+  return max;
+}
 
 module.exports = maxSubarraySum;
