e0b534fdfe4cedfba79e65f51f73ee665d541ce2
Robbie Prokop <robbieprokop@gmail.com>
2023-12-18 19:37:02 -0700

    2 solutions for phone number directory

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,21 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(arr) {
+  const phoneMap = new Map([]);
+
+  for (const numberPair of arr) {
+    const name = numberPair.split(":")[0];
+    const number = numberPair.split(":")[1];
+
+    phoneMap.set(name, number);
+  }
+
+  // arr.forEach((numberPair) => {
+  //   const name = numberPair.split(":")[0];
+  //   const number = numberPair.split(":")[1];
+
+  //   phoneMap.set(name, number);
+  // });
+
+  return phoneMap;
+}
 
 module.exports = phoneNumberDirectory;
