d64868c47139b8577e7f42005849641bb939fa9a
Robbie Prokop <robbieprokop@gmail.com>
2023-12-18 19:32:18 -0700

    second solution

--- 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
+++ 06-hash-tables-maps-sets/04-word-frequency-counter/word-frequency-counter.js
@@ -1,12 +1,28 @@
+// function wordFrequencyCounter(str) {
+//   const map = new Map([]);
+//   if (str.length === 0) return map;
+//   const arr = str
+//     .toLowerCase()
+//     .replace(/[!.?,]/g, "")
+//     .split(" ");
+
+//   arr.forEach((word) => {
+//     if (map.has(word)) {
+//       map.set(word, map.get(word) + 1);
+//       return;
+//     }
+//     map.set(word, 1);
+//   });
+
+//   return map;
+// }
+
 function wordFrequencyCounter(str) {
   const map = new Map([]);
-  if (str.length === 0) return map;
-  const arr = str
-    .toLowerCase()
-    .replace(/[!.?,]/g, "")
-    .split(" ");
+  const arr = str.toLowerCase().split(/\W+/);
 
   arr.forEach((word) => {
+    if (word === "") return;
     if (map.has(word)) {
       map.set(word, map.get(word) + 1);
       return;
