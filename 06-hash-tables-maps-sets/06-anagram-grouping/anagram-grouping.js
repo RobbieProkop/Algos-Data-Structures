9c140a76b36fcf280dcc3e88401880edbaf370c1
Robbie Prokop <robbieprokop@gmail.com>
2023-12-21 14:50:12 -0700

    anagram groupings

--- 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
+++ 06-hash-tables-maps-sets/06-anagram-grouping/anagram-grouping.js
@@ -1,4 +1,20 @@
 function anagramGrouping(arr) {
+  const map = new Map();
+
+  arr.forEach(word => {
+    const sorted = word.split("").sort().join("")
+    if(map.has(sorted)) {
+      map.get(sorted).push(word)
+    } else {
+      map.set(sorted, [word])
+    }
+    console.log('map :>> ', map);
+    console.log('word :>> ',sorted);
+  })
+
+  return Array.from(map.values())
+  
+
   
 }
 
