b17cdb9492536c33658dee7073767a921f905a9b
Robbie Prokop <robbieprokop@gmail.com>
2023-12-22 10:17:37 -0700

    create summetric difference solution

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,22 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+
+  const res = []
+
+  for (const num of set1) {
+    if (!arr2.includes(num) && !res.includes(num)) res.push(num)
+  }
+  for (const num of set2) {
+    if (!arr1.includes(num) && !res.includes(num)) res.push(num)
+
+  }
+
+
+
+
+  console.log('res :>> ', res);
+  return res
+}
 
 module.exports = symmetricDifference;
