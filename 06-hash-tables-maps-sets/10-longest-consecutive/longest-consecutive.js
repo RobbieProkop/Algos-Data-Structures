fd1e9f62a459417b7e2ede14e478a59dd3ec3838
Robbie Prokop <robbieprokop@gmail.com>
2023-12-24 16:22:35 -0700

    Add test for longestConsecutiveSequence function

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,30 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(nums) {
+  let largest = 0;
+
+  let set = new Set(nums);
+
+  for (let i = 0; i < nums.length; i++) {
+
+    if (!set.has(nums[i])) {
+      set.add(nums[i])
+    }
+  }
+
+  for (const num of set) {
+    if (set.has(num + 1)) {
+      let currentNum = num;
+      let currentLength = 1;
+
+      while (set.has(currentNum + 1)) {
+        currentLength++;
+        currentNum++;
+      }
+      largest = Math.max(currentLength, largest)
+    }
+
+  }
+
+  return largest
+}
 
 module.exports = longestConsecutiveSequence;
