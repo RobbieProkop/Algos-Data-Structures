0bf2df647e609787d5e0558c7a43d26456e8171a
Robbie Prokop <robbieprokop@gmail.com>
2023-12-23 22:06:24 -0700

    Add twoSum function and test cases

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,21 @@
-function twoSum() {}
+function twoSum(arr, targetSum) {
+  const seenNums = new Set;
+  for (let i = 0; i < arr.length; i++) {
+    const comp = targetSum - arr[i]
+    // for (let j = 0; j < arr.length; j++) {
+    //   if (arr[j] === comp) {
+    //     return [i, j]
+    //   }
+    // }
+
+    if (seenNums.has(comp)) {
+      return [arr.indexOf(comp), i]
+    }
+
+    seenNums.add(arr[i])
+  }
+
+  return []
+}
 
 module.exports = twoSum;
