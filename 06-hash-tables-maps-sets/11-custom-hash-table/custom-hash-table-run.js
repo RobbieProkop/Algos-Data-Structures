1c1c4dd3cb06f2941b2ce94836391660009f9617
Robbie Prokop <robbieprokop@gmail.com>
2023-12-28 12:01:11 -0700

    create hash and print methods on hashTable

--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
@@ -1,1 +1,11 @@
 const HashTable = require('./custom-hash-table');
+
+
+const myHashTable = new HashTable();
+
+
+// const result = myHashTable._hash("Hi There", myHashTable.limit);
+
+
+// console.log('result :>> ', result);
+myHashTable.print()
--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
@@ -1,3 +1,22 @@
-class HashTable {}
+class HashTable {
+  //changing the limit will change the resulting number  due to the modulus of a new limit -- default is 14
+  constructor(limit = 14) {
+    this.storage = [];
+    this.limit = limit;
+  }
+
+  _hash(key, max) {
+    let hash = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % max;
+  }
+
+  print() {
+    console.log('this.storage :>> ', this.storage);
+  }
+}
 
 module.exports = HashTable;
