8261fdc9e495efb0a2c358b227c7c506e2987b4d
Robbie Prokop <robbieprokop@gmail.com>
2023-12-29 14:22:30 -0700

    solver word instance without hash

--- 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
+++ 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
@@ -1,5 +1,15 @@
 const HashTable = require('./HashTable');
 
-function wordInstanceCounter() {}
+function wordInstanceCounter(str, match) {
+  const lowerArr = str.replace(/[.,$%^;:-?!*]/g, "").toLowerCase().split(" ");
+  let count = 0;
+
+  for (const word of lowerArr) {
+    if (word === match) {
+      count++
+    }
+  }
+  return count
+}
 
 module.exports = wordInstanceCounter;
