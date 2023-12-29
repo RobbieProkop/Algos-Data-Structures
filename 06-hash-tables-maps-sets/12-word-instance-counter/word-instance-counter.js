2135d72bd3b06ba1a5b910a506384ec73a358b20
Robbie Prokop <robbieprokop@gmail.com>
2023-12-29 15:03:16 -0700

    solve with hashTable

--- 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
+++ 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
@@ -1,15 +1,37 @@
 const HashTable = require('./HashTable');
 
 function wordInstanceCounter(str, match) {
-  const lowerArr = str.replace(/[.,$%^;:-?!*]/g, "").toLowerCase().split(" ");
-  let count = 0;
+  const lowerArr = str.toLowerCase().split(/\W+/);
+  const wordFrequency = new HashTable()
+  // if (!wordFrequency.has(match)) return 0;
+  const target = match.toLowerCase();
 
-  for (const word of lowerArr) {
-    if (word === match) {
-      count++
+
+  let count = 0;
+  for (const current of lowerArr) {
+    if (current === "") continue;
+    if (current === wordFrequency.has(current)) {
+      wordFrequency.set(current, wordFrequency.get(current) + 1)
+    } else {
+      wordFrequency.set(current, 1)
+    }
+
+    if (current === target) {
+      count = wordFrequency.get(current)
     }
   }
   return count
+
+  //this is without using the hash
+  // const lowerArr = str.replace(/[.,$%^;:-?!*]/g, "").toLowerCase().split(" ");
+  // let count = 0;
+
+  // for (const word of lowerArr) {
+  //   if (word === match.toLowerCase()) {
+  //     count++
+  //   }
+  // }
+  // return count
 }
 
 module.exports = wordInstanceCounter;
