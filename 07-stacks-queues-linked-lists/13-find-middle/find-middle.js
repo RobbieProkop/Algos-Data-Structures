2bf00085bc8c68e2bd647a7bf47b34c68217bb53
Robbie Prokop <robbieprokop@gmail.com>
2023-12-30 15:38:43 -0700

    find second solution

--- 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
+++ 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
@@ -6,8 +6,27 @@ function findMiddle(list) {
     slow = slow.next;
     fast = fast.next.next;
   }
-
+  if (list.length % 2 === 0) {
+    return slow.next
+  }
   return slow
+
+  // solution #2;
+  // let slow = list.head;
+  // let fast = list.head;
+  // let prev = null;
+
+  // while (fast !== null && fast.next !== null) {
+  //   previous = slow;
+  //   slow = slow.next;
+  //   fast = fast.next.next;
+  // }
+
+  // if (fast === null) {
+  //   return previous.next;
+  // }
+
+  // return slow
 }
 
 module.exports = findMiddle;
