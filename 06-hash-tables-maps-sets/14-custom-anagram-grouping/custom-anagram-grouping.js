752b50643948dadb5802b0c4d049cf2d06e1c815
Robbie Prokop <robbieprokop@gmail.com>
2023-12-29 17:19:42 -0700

    Brad's much clearer solution

--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,31 +1,45 @@
 const HashTable = require('./HashTable');
 
 function anagramGrouping(arr) {
-  const ht = new HashTable();
+  // const ht = new HashTable();
 
+
+  // for (const word of arr) {
+  //   const sorted = word.split("").sort().join("");
+  //   //key will be the sorted letters, value will be the word
+  //   if (ht.has(sorted)) {
+
+  //     const value = ht.get(sorted)
+
+  //     let subArr = []
+  //     for (let i = 0; i < value.length; i++) {
+  //       subArr.push(value[i])
+  //     }
+  //     subArr.push(word)
+  //     ht.set(sorted, subArr)
+
+  //   } else {
+  //     ht.set(sorted, [word])
+  //   }
+
+  // }
+  // const allValues = ht.getValues()
+  // return allValues
+
+  //this is Brad's solution
+  const ht = new HashTable();
 
   for (const word of arr) {
     const sorted = word.split("").sort().join("");
-    //key will be the sorted letters, value will be the word
-    if (ht.has(sorted)) {
 
-      const value = ht.get(sorted)
-
-      let subArr = []
-      for (let i = 0; i < value.length; i++) {
-        subArr.push(value[i])
-      }
-      subArr.push(word)
-      ht.set(sorted, subArr)
-
+    if (ht.get(sorted)) {
+      ht.get(sorted).push(word)
     } else {
       ht.set(sorted, [word])
     }
-
   }
-  const allValues = ht.getValues()
-  return allValues
 
+  return ht.getValues()
 }
 
 module.exports = anagramGrouping;
