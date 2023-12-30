c59f9ade4637a77d6b41ac14a2a56f30d29689b9
Robbie Prokop <robbieprokop@gmail.com>
2023-12-29 19:39:28 -0700

    solve balanced

--- 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis-run.js
+++ 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis-run.js
@@ -1,6 +1,7 @@
 const balancedParenthesis = require('./balanced-parenthesis');
 
-const result1 = balancedParenthesis('()()()');
-const result2 = balancedParenthesis('()(');
+// const result1 = balancedParenthesis('()()()');
+// const result2 = balancedParenthesis('()(');
+const result3 = balancedParenthesis('((');
 
-console.log(result1, result2);
+console.log(result3);
--- 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
+++ 07-stacks-queues-linked-lists/04-balanced-parenthesis/balanced-parenthesis.js
@@ -1,5 +1,26 @@
 const Stack = require('./stack');
 
-function balancedParenthesis() {}
+function balancedParenthesis(str) {
+  const stack = new Stack()
+  for (let i = 0; i < str.length; i++) {
+    console.log(`str[${i}] :>> `, str[i]);
+    if (str[i] === "(") {
+      stack.push(str[i])
+    }
+    // if (stack.isEmpty) return false
+    console.log("empty?? ", str[i] === ")" && stack.isEmpty())
+    if (str[i] === ")" && stack.isEmpty()) {
+      return false
+    } else if (str[i] === ")") {
+      stack.pop()
+    }
+
+  }
+  console.log('stack :>> ', stack);
+  if (!stack.isEmpty() || stack.stack.length !== 0) { return false } else {
+    return true
+  }
+
+}
 
 module.exports = balancedParenthesis;
