f1b7c7a07482877de6e3c28bed1a557056f1d261
Robbie Prokop <robbieprokop@gmail.com>
2023-12-29 17:14:22 -0700

    solution for anagram

--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,5 +1,31 @@
 const HashTable = require('./HashTable');
 
-function anagramGrouping() {}
+function anagramGrouping(arr) {
+  const ht = new HashTable();
+
+
+  for (const word of arr) {
+    const sorted = word.split("").sort().join("");
+    //key will be the sorted letters, value will be the word
+    if (ht.has(sorted)) {
+
+      const value = ht.get(sorted)
+
+      let subArr = []
+      for (let i = 0; i < value.length; i++) {
+        subArr.push(value[i])
+      }
+      subArr.push(word)
+      ht.set(sorted, subArr)
+
+    } else {
+      ht.set(sorted, [word])
+    }
+
+  }
+  const allValues = ht.getValues()
+  return allValues
+
+}
 
 module.exports = anagramGrouping;
