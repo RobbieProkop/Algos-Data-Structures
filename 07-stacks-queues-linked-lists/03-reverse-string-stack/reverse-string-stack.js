daca352a4a50ae290c3245d2750c3b349688dcf6
Robbie Prokop <robbieprokop@gmail.com>
2023-12-29 18:07:24 -0700

    solve reverse string with stack

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,19 @@
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+function reverseStringStack(str) {
+  const stack = new Stack()
+  const noPunc = str.replace(/[.?!$%^&*()":;,]/g, "")
+  for (let i = 0; i < noPunc.length; i++) {
+
+    stack.push(noPunc[i])
+  }
+  let newString = "";
+  for (let i = 0; i < noPunc.length; i++) {
+    newString += stack.peek()
+    stack.pop()
+
+  }
+  return newString
+}
 
 module.exports = reverseStringStack;
