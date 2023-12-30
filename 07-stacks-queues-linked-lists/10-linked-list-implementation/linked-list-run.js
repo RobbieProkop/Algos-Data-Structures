643d0b1e942355caa79ca8fa40f36047877e95ee
Robbie Prokop <robbieprokop@gmail.com>
2023-12-30 14:39:52 -0700

    create linked list class

--- 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
+++ 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
@@ -1,1 +1,15 @@
 const { LinkedList } = require('./linked-list');
+
+const list = new LinkedList();
+
+list.add(100)
+list.add(200)
+list.add(300)
+list.add(400)
+
+list.insertAt(3, "New Node")
+list.removeFrom(3)
+
+list.printAll()
+
+// console.log("get >>", list.get(2))
