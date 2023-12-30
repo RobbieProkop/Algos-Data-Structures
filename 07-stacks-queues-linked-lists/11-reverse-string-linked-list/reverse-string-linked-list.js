3db7d55feca161366f7b2fe595847361d24733a2
Robbie Prokop <robbieprokop@gmail.com>
2023-12-30 15:12:32 -0700

    second solution for reversed linked list

--- 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
+++ 07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
@@ -1,27 +1,39 @@
 const { LinkedList } = require('./linked-list');
 
 function reverseStringLinkedList(str) {
-  const list = new LinkedList()
+  // const list = new LinkedList()
+  // const strArr = str.split("");
 
-  const strArr = str.split("");
+  // strArr.forEach(letter => {
+  //   list.add(letter)
+  // })
 
-  console.log('strArr :>> ', strArr);
+  // let reversed = "";
+  // let i = strArr.length - 1
+  // while (i > -1) {
+  //   const current = list.get(i)
+  //   console.log("current", current)
+  //   reversed += current;
+  //   i--;
+  // }
+  // return reversed
 
-  strArr.forEach(letter => {
-    list.add(letter)
-  })
+  // Solution #2
+  const list = new LinkedList();
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    list.add(str[i]);
+  }
 
   let reversed = "";
-  let i = strArr.length - 1
-  while (i > -1) {
-    const current = list.get(i)
-    console.log("current", current)
-    reversed += current;
-    i--;
+  let current = list.head;
+
+  while (current !== null) {
+    reversed += current.data;
+    current = current.next;
   }
 
   return reversed
-
 
 }
 
