7e70fba1cc1198886273f1b8e5b9a0c72b43b63f
Robbie Prokop <robbieprokop@gmail.com>
2023-12-31 15:18:19 -0700

    printtree, insert tree node

--- 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree-run.js
+++ 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree-run.js
@@ -7,3 +7,23 @@ const { Node, BinarySearchTree } = require('./binary-search-tree');
 //    /
 //   2
 
+// this is depth first. not a BST
+// const root = new Node(10);
+// const node5 = new Node(5);
+// const node15 = new Node(15);
+// const node2 = new Node(2);
+
+// root.left = node5;
+// root.right = node15;
+// node5.left = node2;
+
+// const result = new BinarySearchTree()
+
+const bst = new BinarySearchTree();
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+bst.insert(2)
+
+bst.printTree()
+
--- 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree.js
+++ 08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree.js
@@ -1,5 +1,58 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-class BinarySearchTree {}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(data) {
+    const newNode = new Node(data);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let current = this.root;
+
+      while (true) {
+        if (data < current.data) {
+          if (!current.left) {
+            current.left = newNode
+            return this;
+          }
+          current = current.left;
+        }
+        if (data >= current.data) {
+          if (!current.right) {
+            current.right = newNode;
+            return this;
+          }
+          current = current.right;
+        }
+      }
+    }
+  }
+
+  lookup(data) {
+    if (this.root.data === data) return true
+
+  }
+
+  printTree() {
+
+    const printNode = (node) => {
+      if (node === null) return
+      printNode(node.left);
+      console.log(node.data)
+      printNode(node.right);
+    }
+    printNode(this.root)
+  }
+}
 
 module.exports = { Node, BinarySearchTree };
