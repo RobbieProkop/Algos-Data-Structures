102598e7d46997e3620ea6960639af93e66d87ed
Robbie Prokop <robbieprokop@gmail.com>
2023-12-31 13:59:09 -0700

    max depth solution 1

--- 08-binary-trees-graphs/06-maximum-depth/maximum-depth-run.js
+++ 08-binary-trees-graphs/06-maximum-depth/maximum-depth-run.js
@@ -5,3 +5,18 @@ const { Node, maxDepth } = require('./maximum-depth');
 //                          9  20
 //                             / \
 //                            15  7
+
+const root = new Node('3');
+const b = new Node('9');
+const c = new Node('20');
+const d = new Node('15');
+const e = new Node('7');
+
+root.left = b;
+root.right = c;
+c.left = d;
+c.right = e;
+
+const result = maxDepth(root);
+
+console.log("result", result)
