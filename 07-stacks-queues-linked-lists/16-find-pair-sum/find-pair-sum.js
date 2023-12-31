30512ca140b4552966774e09fec89a306dfdffcd
Robbie Prokop <robbieprokop@gmail.com>
2023-12-30 18:19:59 -0700

    find pair sum

--- 07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
+++ 07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
@@ -1,5 +1,16 @@
 const DoublyLinkedList = require('./DoublyLinkedList');
 
-function findPairSum() {}
+function findPairSum(nums, target) {
+
+  const list = new DoublyLinkedList()
+  for (const num of nums) {
+    const difference = target - num;
+
+    if (list.contains(difference)) return [difference, num]
+    list.append(num);
+  }
+
+  return null
+}
 
 module.exports = findPairSum;
