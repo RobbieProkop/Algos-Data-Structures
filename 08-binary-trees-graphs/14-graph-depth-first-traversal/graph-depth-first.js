1efaec676ef4abaebb1567b6ff4f1c7cfb497718
Robbie Prokop <robbieprokop@gmail.com>
2024-01-01 22:39:27 -0700

    add recursive example

--- 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
+++ 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
@@ -1,34 +1,55 @@
 const Stack = require('./stack');
 
+//Non Recursively
+// function depthFirstTraversal(graph, root) {
+//   if (!graph.adjacencyList[root]) return []
+
+//   const visited = {};
+//   const stack = new Stack();
+//   const result = [];
+
+//   stack.push(root);
+//   visited[root] = true;
+
+//   while (!stack.isEmpty()) {
+//     const current = stack.pop();
+//     console.log('current :>> ', current);
+//     result.push(current)
+
+//     graph.adjacencyList[current].forEach((neighbour) => {
+//       if (!visited[neighbour]) {
+//         visited[neighbour] = true;
+//         stack.push(neighbour);
+//       }
+//     })
+
+//     // for (let i = 0; i < graph.adjacencyList[current].length; i++) {
+//     //   if (!visited[graph.adjacencyList[current][i]]) {
+//     //     visited[graph.adjacencyList[current][i]] = true;
+//     //     stack.push(graph.adjacencyList[current][i]);
+//     //   }
+//     // }
+//   }
+//   return result
+// }
+
 function depthFirstTraversal(graph, root) {
   if (!graph.adjacencyList[root]) return []
 
   const visited = {};
-  const stack = new Stack();
   const result = [];
 
-  stack.push(root);
   visited[root] = true;
 
-  while (!stack.isEmpty()) {
-    const current = stack.pop();
-    console.log('current :>> ', current);
-    result.push(current)
-
-    graph.adjacencyList[current].forEach((neighbour) => {
-      if (!visited[neighbour]) {
-        visited[neighbour] = true;
-        stack.push(neighbour);
-      }
+  function dfs(vertex) {
+    visited[vertex] = true
+    result.push(vertex)
+    graph.adjacencyList[vertex].forEach(neighbour => {
+      if (!visited[neighbour]) { dfs(neighbour) }
     })
-
-    // for (let i = 0; i < graph.adjacencyList[current].length; i++) {
-    //   if (!visited[graph.adjacencyList[current][i]]) {
-    //     visited[graph.adjacencyList[current][i]] = true;
-    //     stack.push(graph.adjacencyList[current][i]);
-    //   }
-    // }
   }
+  dfs(root)
+
   return result
 }
 
