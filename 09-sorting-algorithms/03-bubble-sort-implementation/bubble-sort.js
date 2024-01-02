ec2532193584836b7b17713d79678d1427a48d36
Robbie Prokop <robbieprokop@gmail.com>
2024-01-01 23:26:52 -0700

    basic bubble sort

--- 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
+++ 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
@@ -1,3 +1,14 @@
-function bubbleSort() {}
+function bubbleSort(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length - i - 1; j++) {
+      if (nums[j] > nums[j + 1]) {
+        const temp = nums[j]
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp
+      }
+    }
+  }
+  return nums
+}
 
 module.exports = bubbleSort;
