0b1e561636006ab83319576e604870861b3ce38b
Robbie Prokop <robbieprokop@gmail.com>
2024-01-01 23:36:22 -0700

    Refactor bubbleSort function and add test cases

--- 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
+++ 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
@@ -1,14 +1,32 @@
-function bubbleSort(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length - i - 1; j++) {
-      if (nums[j] > nums[j + 1]) {
-        const temp = nums[j]
-        nums[j] = nums[j + 1];
-        nums[j + 1] = temp
+// function bubbleSort(nums) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = 0; j < nums.length - i - 1; j++) {
+//       if (nums[j] > nums[j + 1]) {
+//         const temp = nums[j]
+//         nums[j] = nums[j + 1];
+//         nums[j + 1] = temp
+//       }
+//     }
+//   }
+//   return nums
+// }
+
+function bubbleSort(arr) {
+  let swapped = false;
+
+  while (!swapped) {
+    swapped = true;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = false
       }
     }
   }
-  return nums
+  return arr
 }
 
 module.exports = bubbleSort;
