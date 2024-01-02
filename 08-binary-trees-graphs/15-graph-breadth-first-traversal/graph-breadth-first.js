b20f84c814c39d5516f5bb760c9d6368180d9555
Robbie Prokop <robbieprokop@gmail.com>
2024-01-01 23:04:27 -0700

    2 different breath traversal solutions

--- 08-binary-trees-graphs/15-graph-breadth-first-traversal/graph-breadth-first.js
+++ 08-binary-trees-graphs/15-graph-breadth-first-traversal/graph-breadth-first.js
@@ -1,5 +1,50 @@
 const Queue = require('./queue');
 
-function breadthFirstTraversal() {}
+// function breadthFirstTraversal(g, vertex) {
+//   if (!vertex) return [];
+
+//   const visited = {};
+//   const result = [];
+//   const queue = new Queue();
+
+//   queue.enqueue(vertex);
+//   visited[vertex] = true
+
+//   while (!queue.isEmpty()) {
+//     const current = queue.dequeue();
+//     result.push(current)
+//     console.log('result :>> ', result);
+//     g.adjacencyList[current].forEach(neighbour => {
+//       if (!visited[neighbour]) {
+//         visited[neighbour] = true;
+//         queue.enqueue(neighbour)
+//       }
+//     })
+//   }
+//   return result
+// }
+
+function breadthFirstTraversal(g, vertex) {
+  if (!vertex) return [];
+
+  const visited = new Set();
+  const result = [];
+  const queue = new Queue();
+
+  queue.enqueue(vertex);
+  visited.add(vertex);
+
+  while (!queue.isEmpty()) {
+    const current = queue.dequeue();
+    result.push(current);
+    for (const neighbour of g.adjacencyList[current]) {
+      if (!visited.has(neighbour)) {
+        visited.add(neighbour);
+        queue.enqueue(neighbour);
+      }
+    }
+  }
+  return result
+}
 
 module.exports = breadthFirstTraversal;
