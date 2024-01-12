0bb210ab14eb52695a97f51b70ba2b5dea7a337a
Robbie Prokop <robbieprokop@gmail.com>
2024-01-11 23:16:05 -0700

    solve minor bug

--- 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
+++ 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
@@ -1,5 +1,5 @@
 function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length - 1; i++) {
     let minIndex = i;
 
     for (let j = i + 1; j < arr.length; j++) {
